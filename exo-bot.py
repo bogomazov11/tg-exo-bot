91d40264fe84fad4f422bd6a5f763f224eecacc3
bogomazov11 <alexander.bogomazov123@gmail.com>
2024-02-24 08:43:02 +0300

    sCOMM

--- exo-bot.py
+++ exo-bot.py
@@ -1,13 +1,22 @@
 import telebot 
-token = '6792331573:AAF9q4_lXwzorboMwF9s6B8TbDthQAZ-tZI'
-bot= telebot.TeleBot(token)
+token = '6792331573:AAF9q4_lXwzorboMwF9s6B8TbDthQAZ-tZI' # Токен указан, чтобы бот работал в телеграмме
+bot= telebot.TeleBot(token) # Здесь указываем боту на его токен
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start']) # Действие бота при комманде /start
 def start_message(m, res=False):
     bot.send_message(m.chat.id, 'Приветствую, напишите мне что-нибудь')
 
-@bot.message_handler(content_types='text')
+@bot.message_handler(commands=['help']) # Действие при комманде /help
+def start_message(m, res=False):
+    bot.send_message(m.chat.id, 'Просто напишите какой-нибудь текст')
+
+@bot.message_handler(content_types=['text'])
 def message_reply(message):
-    bot.send_message(message.chat.id, 'По-моему, ' + message.text)
+    if message.text == 'Хватит повторять' or message.text == 'хватит повторять':
+        bot.send_message(message.chat.id, 'ну ладно((')
+    elif message.text == 'Перестань' or message.text == 'перестань':
+        bot.send_message(message.chat.id, 'Не могу')
+    else:
+        bot.send_message(message.chat.id, 'Мне показалось? Или ' + message.text)
 
 bot.polling(none_stop=True, interval=0)
